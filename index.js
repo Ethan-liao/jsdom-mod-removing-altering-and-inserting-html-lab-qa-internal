55b1923e5b1ca1b9231051b28fca4131879a11e8
Ethan-liao <ethan-liao@outlook.com>
2018-09-05 22:31:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Steven is the champion!";
+newHeader.className = "victory";
