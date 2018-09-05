c35485f7f9b5289df3cf1c2f8fd0df1c75230479
Ethan-liao <ethan-liao@outlook.com>
2018-09-05 22:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 
+document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 
 const newHeader = document.createElement("h1");
