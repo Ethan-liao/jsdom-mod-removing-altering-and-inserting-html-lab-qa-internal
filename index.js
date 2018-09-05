62d404295b9036f97545703ece92cf9ee5b8d403
Ethan-liao <ethan-liao@outlook.com>
2018-09-05 22:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
 var element = document.createElement('div')
 /* Create your new element here and assign it to newHeader */
-
+document.body.appendChild(element)
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerText = "Steven is the champion!";
