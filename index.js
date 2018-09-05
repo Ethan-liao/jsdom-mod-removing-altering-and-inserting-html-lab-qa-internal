5fbc983ec207a8e8f8f1f16e51f0eaa94127822e
Ethan-liao <ethan-liao@outlook.com>
2018-09-05 22:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-var element = document.createElement('div')
-element.appendChild(ul)
-document.body.appendChild(element)
-
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
